9255a55a25f92b5aebf70a32370ab2ea07403e93
KentaroOka-TechBase <Kentaro.Oka@jp.nttdata.com>
2025-10-30 09:15:18 +0000

    EnvViewerのタグの修正

--- scenario-nextjs-env/next-env-demo/src/components/EnvViewer.tsx
+++ scenario-nextjs-env/next-env-demo/src/components/EnvViewer.tsx
@@ -15,15 +15,15 @@ export default function EnvViewer() {
     <div>
       <p>
         <strong>Client-side (CLIENT_MESSAGE):</strong>{" "}
-        <p>この演習の名前は{process.env.CLIENT_MESSAGE ?? "(not available)"}です</p>
+        この演習の名前は<span>{process.env.CLIENT_MESSAGE ?? "(not available)"}</span>です
       </p>
       <p>
         <strong>Client-side (CLIENT_MESSAGE):</strong>{" "}
-          <p>この演習の名前は{process.env.NEXT_PUBLIC_CLIENT_MESSAGE ?? "(not available)"}です</p>
+          <span>この演習の名前は{process.env.NEXT_PUBLIC_CLIENT_MESSAGE ?? "(not available)"}</span>です
       </p>
       <p>
         <strong>Server-side (SECRET_MESSAGE):</strong>{" "}
-        <p>この演習の名前は{serverMessage ?? "(loading...)"}です</p>
+        <span>この演習の名前は{serverMessage ?? "(loading...)"}</span>です
       </p>
     </div>
   );
